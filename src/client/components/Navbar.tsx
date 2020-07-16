41849bf437cd7cff42d4ec45f3001ee4229d44e0
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-07-16 02:40:37 -0500

    style updated

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
+
 class Navbar extends React.Component<NavbarProps, NavbarState> {
     render(){
         return(
-            <nav className="nav justify-content-center border-bottom border-dark p-2">
+            <nav className="nav justify-content-center border-bottom border-success p-3">
                 <NavLink className="text-decoration-none btn btn-link" activeClassName="border-bottom border-primary" exact to="/">Home</NavLink>
                 <NavLink className="text-decoration-none btn btn-link" activeClassName="border-bottom border-primary" exact to="/compose">Compose</NavLink>
             </nav>
